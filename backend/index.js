2d040f3fe5041e29be9d20f4f973287b4282135c
dlshnkLK <dilshankaliyanage@yahoo.com>
2023-10-24 11:11:22 +0530

    init

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import { PORT } from './config.js';
+
+const app = express();
+
+app.listen(PORT, () => {
+    console.log(`App is running on port: ${PORT}`);
+});
+
+
+
+
