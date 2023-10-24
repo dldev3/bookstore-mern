5ea591f1b94edf2f9ef7bc1eb5658e1853320f3b
dlshnkLK <dilshankaliyanage@yahoo.com>
2023-10-24 13:05:52 +0530

    pswd changed

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
 export const PORT = 5555;
 
-export const mongoUrl = 'mongodb+srv://araliyamapa85:d12345678@bookstore-mern.jdbohjf.mongodb.net/books-collection?retryWrites=true&w=majority'
+export const mongoUrl = 'mongodb+srv://araliyamapa85:d12345678ddd@bookstore-mern.jdbohjf.mongodb.net/books-collection?retryWrites=true&w=majority'
